e81d6203b127eb392dd7d2bf4c2b171afa339cb4
Iain <45532247+Arrowerse2001@users.noreply.github.com>
2019-04-04 16:25:19 +0100

    Letterboxd

--- Letterboxd/main.js
+++ Letterboxd/main.js
@@ -1,36 +1,32 @@
 const electron = require('electron');
 const url = require('url');
 const path = require('path');
-const naiveImage = electron.nativeImage;
-
-
 
 const { app, BrowserWindow, Menu, ipcMain } = electron;
 
-
-
-
-
 let mainWindow;
 
+
+
+
+
 app.on('ready', function() {
-    mainWindow = new BrowserWindow({ 
-        webPreferences: {
-            plugins: true,
-            sandbox: true,
-            nodeIntegration: false,
-        }
+    mainWindow = new BrowserWindow({titleBarStyle: 'hidden',
+     width: 1281,
+     height: 800,
+     autoHideMenuBar: true,
+     frame: false,
+     webPreferences: {
+         plugins: true,
+         sandbox: true,
+         nodeIntegration: false,
+     }
+     
     });
-
-   
-
+    
     mainWindow.loadURL('http://www.letterboxd.com');
 
-    mainWindow.on('closed', function(){
-        app.quit();
-    });
+    
 
     
 })
-
-
